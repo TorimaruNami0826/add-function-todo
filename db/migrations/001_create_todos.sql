30864532881a989c688e3f1838890e48c0be9513
Torimaru Nami <n-torimaru@holon.co.jp>
2026-04-30 16:15:55 +0900

    add migration file

---
+++ db/migrations/001_create_todos.sql
@@ -0,0 +1,20 @@
+CREATE TABLE todos (
+id         BIGSERIAL   PRIMARY KEY,
+title      TEXT        NOT NULL,
+completed  BOOLEAN     NOT NULL DEFAULT false,
+created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE OR REPLACE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+NEW. updated_at = NOW();
+RETURN NEW;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER todos_updated_at
+BEFORE UPDATE ON todos
+FOR EACH ROW EXECUTE FUNCTION update_updated_at();
